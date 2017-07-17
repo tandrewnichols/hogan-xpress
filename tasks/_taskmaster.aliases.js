a582da1f274f15979317ca4afbff7066f8cb2cec
Andrew Nichols <tandrewnichols@gmail.com>
2017-07-17 17:18:07 -0400

    Run different task based on node version

--- tasks/_taskmaster.aliases.js
+++ tasks/_taskmaster.aliases.js
@@ -2,5 +2,5 @@ module.exports = {
   mocha: ['mochaTest:test'],
   default: ['eslint:lib', 'mocha'],
   coverage: ['istanbul:unit', 'open:coverage'],
-  ci: ['default', 'travisMatrix']
+  ci: ['eslint:lib', 'travisMatrix']
 };
--- tasks/travisMatrix.js
+++ tasks/travisMatrix.js
@@ -1,8 +1,14 @@
 module.exports = {
-  v4: {
+  v6: {
     test: function() {
       return /^v6/.test(process.version);
     },
     tasks: ['istanbul:unit', 'shell:codeclimate']
+  },
+  other: {
+    test: function() {
+      return !/^v6/.test(process.version);
+    },
+    tasks: ['mocha']
   }
 };
